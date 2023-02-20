d951d78fc6cedc4e22c495122a0d6554b36c763f
root <root@ip-172-31-62-183.ec2.internal>
2023-02-20 13:48:17 +0000

    2nd commit

--- samplescript.sh
+++ samplescript.sh
@@ -2,7 +2,8 @@
 PH=$(curl -sL http://169.254.169.254/latest/meta-data/public-hostname)
 SG=$(curl -sL http://169.254.169.254/latest/meta-data/security-groups)
 ID=$(curl -sL http://169.254.169.254/latest/meta-data/instance-id)
-
+IT=$(curl -sL http://169.254.169.254/latest/meta-data/instance-type)
 echo "$PH"
 echo "$SG"
 echo "$ID"
+echo "$IT"
